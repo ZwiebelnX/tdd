1875c773b1d347fff9462d63c83e6abc01bf83bc
Chen Sicong <sicong.chen@163.com>
2020-08-03 22:21:27 +0800

    [Chen Sicong] test: simplify test case

--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -12,24 +12,13 @@ public class MommifierTest {
     }
     @Test
     void should_return_origin_string_when_mommify_given_normal_strings() throws Exception {
-        String result = mommifier.mommify("12345");
-        assertEquals("12345", result);
-        result = mommifier.mommify("!@#$%^&");
+        String result = mommifier.mommify("!@#$%^&");
         assertEquals("!@#$%^&", result);
-        result = mommifier.mommify("apple");
-        assertEquals("apple", result);
-        result = mommifier.mommify("");
-        assertEquals("", result);
+
     }
     @Test
     void should_return_mommifier_string_when_mommify_given_specific_strings() throws Exception {
-        String result = mommifier.mommify("afei");
-        assertEquals("afemommyi", result);
-        result = mommifier.mommify("aeixxxei");
-        assertEquals("amommyemommyixxxemommyi", result);
-        result = mommifier.mommify("aaaaaaxxxx");
-        assertEquals("amommyamommyamommyamommyamommyaxxxx", result);
-        result = mommifier.mommify("00aieou00");
+        String result = mommifier.mommify("00aieou00");
         assertEquals("00amommyimommyemommyomommyu00", result);
     }
 
