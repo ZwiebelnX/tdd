120bb687730ebb797215a622b33057f0e0fc7cbc
Chen Sicong <sicong.chen@163.com>
2020-08-03 17:14:34 +0800

    [Chen Sicong] feat: add test and Mommifier.java

---
+++ src/main/java/Mommifier.java
@@ -0,0 +1,6 @@
+public class Mommifier {
+
+    public String convert(String s) {
+        return s;
+    }
+}
--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -1,2 +1,18 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MommifierTest {
+    private Mommifier mommifier;
+
+    @BeforeEach
+    void setUp() {
+        this.mommifier = new Mommifier();
+    }
+    @Test
+    void should_return_origin_string_when_convert_given_normal_string() {
+        String result = mommifier.convert("12345");
+        assertEquals("12345", result);
+    }
 }
